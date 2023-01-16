7e4344ebb13ff436ffaa5dded7c3597fd7e156ef
ahmed behalil mohamed amine <ahmedbehalil16@gmail.com>
2023-01-16 21:43:06 +0100

    correction

--- exo_07_a.rb
+++ exo_07_a.rb
@@ -1,3 +1,3 @@
 puts "Bonjour, c'est quoi ton blase ?"
-user_name = gets.chomp
-puts user_name
+user_name = gets.chomp #gets chomp nous donne la main pour repondre a la question posé avec le puts 
+puts user_name #notre reponse est affiché ici car on a donner une valeur a la variable avec getschomp
