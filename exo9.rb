88c77fc522f01c4a95db38b63d13f52dc23624b0
ahmed behalil mohamed amine <ahmedbehalil16@gmail.com>
2023-01-16 22:22:06 +0100

    commit 9

---
+++ exo9.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, quel est ton prénom?"
+print "Ecris ton prénom ici:"
+user_name = gets.chomp
+puts "Bonjour, quel est ton nom?"
+print "Ecris ton nom ici:"
+name = gets.chomp
+puts "Bonjour, #{user_name} #{name}!"
--- exo_08.rb
+++ exo_08.rb
@@ -1,4 +1,8 @@
-puts "Bonjour, quel est ton prénom?"
-print "Ecris ton prénom ici:"
-user_name = gets.chomp
-puts "Bonjour, #{user_name} !"
+puts "Bonjour, quel est ton prénom et ton nom?"
+print "Ecris ton prénom  et ici:"
+user_name  and name = gets.chomp
+
+puts "Bonjour, quel est ton nom?"
+print "Ecris ton nom ici:"
+name = gets.chomp
+puts "Bonjour, #{user_name and name }"
