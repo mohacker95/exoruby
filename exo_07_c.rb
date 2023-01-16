7cd855b7fecd8c1fdb956a082e39422f008468aa
ahmed behalil mohamed amine <ahmedbehalil16@gmail.com>
2023-01-16 22:00:15 +0100

    exo7

--- exo_07_c.rb
+++ exo_07_c.rb
@@ -1,2 +1,7 @@
 user_name = gets.chomp
 puts user_name
+
+
+#la diff entre les 3 exo dans le premier le puts"" affiche un texte  nous pose une question ,et le getschomp nous permet de donner une repose et apres le puts affiche notre reponse ,
+#dans B  on rajoute juste le > tout simplement 
+# dans C on a retirer les textes donc on donne juste la reponse et le code l'affiche 
