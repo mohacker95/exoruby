0c520f03f7bc0ba5f469ab85c15b800f1bbf256c
ahmed behalil mohamed amine <ahmedbehalil16@gmail.com>
2023-01-16 21:34:36 +0100

    crt

--- exo_06.rb
+++ exo_06.rb
@@ -5,3 +5,5 @@ number_of_minutes_in_an_hour =60
 
 puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
 puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# les variables sont multipliées pour "travail" avec les chiffres qui ont été attribués au debut 
